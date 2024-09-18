0161fca04e84fe905e8728284ed86bd228359484
Batuhan Akkaya <v-batuhan.akkaya@kajabi.com>
2024-09-18 22:00:05 +0300

    Initial Commit

---
+++ mbanative/mbanativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mbanativeApp.swift
+//  mbanative
+//
+//  Created by Batuhan on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct mbanativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
